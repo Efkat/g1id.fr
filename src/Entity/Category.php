95e9011a303df755235178051c000038c83c56aa
Efkat <hugo.ben80000@gmail.com>
2021-04-05 17:17:31 +0200

    Add : project entity and relation with category

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -39,11 +39,17 @@ class Category
      */
     private $exercices;
 
+    /**
+     * @ORM\OneToMany(targetEntity=Project::class, mappedBy="Category")
+     */
+    private $projects;
+
     public function __construct()
     {
         $this->activities = new ArrayCollection();
         $this->courses = new ArrayCollection();
         $this->exercices = new ArrayCollection();
+        $this->projects = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -147,6 +153,36 @@ class Category
             // set the owning side to null (unless already changed)
             if ($exercice->getCategory() === $this) {
                 $exercice->setCategory(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Project[]
+     */
+    public function getProjects(): Collection
+    {
+        return $this->projects;
+    }
+
+    public function addProject(Project $project): self
+    {
+        if (!$this->projects->contains($project)) {
+            $this->projects[] = $project;
+            $project->setCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function removeProject(Project $project): self
+    {
+        if ($this->projects->removeElement($project)) {
+            // set the owning side to null (unless already changed)
+            if ($project->getCategory() === $this) {
+                $project->setCategory(null);
             }
         }
 
