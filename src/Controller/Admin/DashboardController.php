f57a90818957b2d478932f44ba29695b36351d90
Efkat <hugo.ben80000@gmail.com>
2021-04-09 15:16:58 +0200

    Add : SFEBundle

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -7,13 +7,17 @@ use App\Entity\Exercice;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
 
 class DashboardController extends AbstractDashboardController
 {
     /**
      * @Route("/dashboard", name="admin")
+     * @IsGranted("ROLE_ADMIN")
      */
     public function index(): Response
     {
