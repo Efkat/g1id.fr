10465b1e591306fb57bf90ff7a95e07b445909ce
Efkat <hugo.ben80000@gmail.com>
2021-04-09 11:46:13 +0200

    Revert "Implement : Admin Dashboard"
    This reverts commit a6b7c241a7a48593a5f3821803cc9e750fb19d56.

--- config/bundles.php
+++ config/bundles.php
@@ -9,5 +9,4 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
-    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
