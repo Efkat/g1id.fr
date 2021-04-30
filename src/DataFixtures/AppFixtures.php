6789d1b9843998b7c3ff0ca713c29d738a4259d3
Efkat <hugo.ben80000@gmail.com>
2021-04-30 15:16:17 +0200

    Add : Exercice Fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\Category;
 use App\Entity\Course;
+use App\Entity\Exercice;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
@@ -14,6 +15,8 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         $faker = Factory::create("fr_FR");
+
+        //Création des fixtures catégories
         $categories = [];
         for($i=0;$i<5;$i++){
             $category = new Category();
@@ -21,7 +24,10 @@ class AppFixtures extends Fixture
             array_push($categories, $category);
             $manager->persist($category);
         }
+        //Création de la référence sur une catégorie
         $this->addReference("category", $categories[$faker->numberBetween(1,5)]);
+
+        //Création des fixtures Cours
         for($i = 0; $i<100;$i++){
             $course = new Course();
             $course
@@ -30,6 +36,19 @@ class AppFixtures extends Fixture
                 ->setDifficulty($faker->numberBetween(1, 5))
                 ->setCategory($this->getReference("category"));
             $manager->persist($course);
+        }
+
+        //Création des fixtures exercices
+        for($i = 0; $i<100;$i++){
+            $exercice = new Exercice();
+            $exercice
+                ->setTitle($faker->words(3, true))
+                ->setContent($faker->sentences(4, true))
+                ->setHelp($faker->sentences(4, true))
+                ->setCorrection($faker->sentences(4, true))
+                ->setDifficulty($faker->numberBetween(1, 5))
+                ->setCategory($this->getReference("category"));
+            $manager->persist($exercice);
         }
         $manager->flush();
 
