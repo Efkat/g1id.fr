e3290c3bb861db1cdfea2606202f81efa4ec4ae4
Efkat <hugo.ben80000@gmail.com>
2021-05-03 13:08:45 +0200

    Add : Chapters Fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,13 +3,17 @@
 namespace App\DataFixtures;
 
 use App\Entity\Activity;
+use App\Entity\ActivityChapter;
 use App\Entity\Category;
 use App\Entity\Course;
+use App\Entity\CourseChapter;
 use App\Entity\Exercice;
 use App\Entity\Project;
+use App\Entity\ProjectChapter;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
+use Gedmo\Mapping\Driver\Chain;
 
 class AppFixtures extends Fixture
 {
@@ -29,18 +33,32 @@ class AppFixtures extends Fixture
         $this->addReference("category", $categories[$faker->numberBetween(1,5)]);
 
         //Création des fixtures Cours
-        for($i = 0; $i<50;$i++){
+        for($i = 0; $i<20;$i++){
             $course = new Course();
+            $chapitres = [];
             $course
                 ->setTitle($faker->words(3, true))
                 ->setSummary($faker->sentences(4, true))
                 ->setDifficulty($faker->numberBetween(1, 5))
                 ->setCategory($this->getReference("category"));
             $manager->persist($course);
+
+            for($j=0; $j<3;$j++){
+                $chapitre = new CourseChapter();
+                $chapitre
+                    ->setTitle($faker->words(3, true))
+                    ->setTime($faker->numberBetween(5, 30))
+                    ->setContent($faker->sentences(12, true));
+                array_push($chapitres, $chapitre);
+                $manager->persist($chapitre);
+            }
+            foreach ($chapitres as $chapitre) {
+                $chapitre->setCourse($course);
+            }
         }
 
         //Création des fixtures exercices
-        for($i = 0; $i<50;$i++){
+        for($i = 0; $i<20;$i++){
             $exercice = new Exercice();
             $exercice
                 ->setTitle($faker->words(3, true))
@@ -53,24 +71,54 @@ class AppFixtures extends Fixture
         }
 
         //Création des fixtures Activités
-        for($i = 0; $i<50;$i++){
+        for($i = 0; $i<20;$i++){
             $activity = new Activity();
+            $chapitres = [];
             $activity
                 ->setTitle($faker->words(3, true))
                 ->setSummary($faker->sentences(4, true))
                 ->setDifficulty($faker->numberBetween(1, 5))
                 ->setCategory($this->getReference("category"));
             $manager->persist($activity);
+
+            //Création des chapitres par activité
+            for($j=0; $j<3;$j++){
+                $chapitre = new ActivityChapter();
+                $chapitre
+                    ->setTitle($faker->words(3, true))
+                    ->setTime($faker->numberBetween(5, 30))
+                    ->setContent($faker->sentences(12, true));
+                array_push($chapitres, $chapitre);
+                $manager->persist($chapitre);
+            }
+            foreach ($chapitres as $chapitre) {
+                $chapitre->setActivity($activity);
+            }
         }
 
         //Création des fixtures projets
-        for($i = 0; $i<50;$i++){
+        for($i = 0; $i<20;$i++){
             $project = new Project();
+            $chapitres = [];
             $project
                 ->setTitle($faker->words(3, true))
                 ->setSummary($faker->sentences(4, true))
                 ->setDifficulty($faker->numberBetween(1, 5))
                 ->setCategory($this->getReference("category"));
+
+            //Création des chapitres PAR projets
+            for($j=0; $j<3;$j++){
+                $chapitre = new ProjectChapter();
+                $chapitre
+                    ->setTitle($faker->words(3, true))
+                    ->setTime($faker->numberBetween(5, 30))
+                    ->setContent($faker->sentences(12, true));
+                array_push($chapitres, $chapitre);
+                $manager->persist($chapitre);
+            }
+            foreach ($chapitres as $chapitre) {
+                $chapitre->setProject($project);
+            }
             $manager->persist($project);
         }
         $manager->flush();
